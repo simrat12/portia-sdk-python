6d093f145ca9271bd517e2fad3aba372feb2efe1
robbie-portia <robbie@portialabs.ai>
2025-02-17 17:48:02 +0000

    Reduce LLM broader context (#134)
    * Reduce LLM broader context
    
    * Fix test

--- portia/agents/context.py
+++ portia/agents/context.py
@@ -85,10 +85,13 @@ def generate_input_context(
             "indirectly help you.",
         )
         for output_key in unused_output_keys:
+            # We truncate the output value to 1000 characters to avoid overwhelming the
+            # LLM with too much information.
+            output_val = (str(previous_outputs[output_key].value) or "")[:1000]
             input_context.extend(
                 [
                     f"output_name: {output_key}",
-                    f"output_value: {previous_outputs[output_key].value}",
+                    f"output_value: {output_val}",
                     "----------",
                 ],
             )
